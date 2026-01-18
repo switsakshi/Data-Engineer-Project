80caf8660bb5a34de0664213ac69833dd8b5c768
switsakshi <85685775+switsakshi@users.noreply.github.com>
2026-01-18 18:15:58 +0530

    Update main.py

--- Learn-Google-Cloud-master/YouTube Demos/App Engine/Standard/main.py
+++ Learn-Google-Cloud-master/YouTube Demos/App Engine/Standard/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World Welcome to TechTrapture from Standard Environment - Version 3!!'
+    return 'Hello, World Welcome to my application from Standard Environment - Version 1!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
