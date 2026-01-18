c4c11e3245b97c642960327549fb5f7bdebf2a56
switsakshi <85685775+switsakshi@users.noreply.github.com>
2026-01-18 18:21:18 +0530

    Update main.py

--- Learn-Google-Cloud-master/YouTube Demos/Cloud Function/main.py
+++ Learn-Google-Cloud-master/YouTube Demos/Cloud Function/main.py
@@ -10,13 +10,13 @@ def check_file_size_and_move(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
-    source_bucket_name = event['bucket']
+    source_bucket_name = event['bkt-src-global-data']
     file_name = event['name']
 
     # Initialize GCS client
     storage_client = storage.Client()
-    source_bucket = storage_client.bucket(source_bucket_name)
-    blob = source_bucket.blob(file_name)
+    source_bucket = storage_client.bucket(bkt-src-global-data)
+    blob = source_bucket.blob(global_health_data.csv)
 
     # Reload blob metadata to ensure size is fetched
     blob.reload()
@@ -26,7 +26,7 @@ def check_file_size_and_move(event, context):
 
     # Ensure file size is not None
     if file_size is None:
-        print(f"Unable to determine the size for {file_name}. Skipping.")
+        print(f"Unable to determine the size for {global_health_data.csv}. Skipping.")
         return
 
     max_size = 2 * 1024 * 1024  # 2MB in bytes
@@ -34,12 +34,12 @@ def check_file_size_and_move(event, context):
     if file_size > max_size:
         # Move the file to the "large_files/" folder in the destination bucket
         destination_bucket = storage_client.bucket(INVALID_FILES_BUCKET)
-        new_blob_name = f"large_files/{file_name}"  # Add folder prefix
+        new_blob_name = f"large_files/{global_health_data.csv}"  # Add folder prefix
         new_blob = source_bucket.copy_blob(blob, destination_bucket, new_blob_name)
            
         # Delete the original file
         blob.delete()
 
-        print(f"File {file_name} moved to {INVALID_FILES_BUCKET}/large_files/{file_name} because it exceeds 2MB.")
+        print(f"File {global_health_data.csv} moved to {bkt-cfdemo-dest-000}/large_files/{global_health_data_large.csv} because it exceeds 2MB.")
     else:
-        print(f"File {file_name} is within the size limit (<= 2MB). No action taken.")
+        print(f"File {global_health_data.csv is within the size limit (<= 2MB). No action taken.")
